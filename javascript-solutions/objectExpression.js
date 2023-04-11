175ed4309b6a6e4377d4b9457360e39272ec53bc
Diakov Maksim <maksim2004d@yandex.ru>
2023-04-11 23:56:30 +0300

    Add 7 and 8 HWs

--- javascript-solutions/objectExpression.js
+++ javascript-solutions/objectExpression.js
@@ -1,111 +1,70 @@
 "use strict"
-//:NOTE: use semicolons `;`
-
-const operations = new Map([])
-
-//:NOTE: arr.join(" ");
-function ArrToStr(arr) {
-    let s = ""
-    while (arr.length > 0) {
-        s += arr.shift()
-    }
-    return s
-}
+const operations = new Map([]);
+const variables = new Map([
+    ["x", 0],
+    ["y", 1],
+    ["z", 2]
+]);
 
 function Const(value) {
-    //:NOTE: .toString()
-    this.toString = () => (String)(value)
-    this.evaluate = () => value
-    this.prefix = this.toString
+    this.toString = () => (value).toString();
+    this.evaluate = () => value;
+    this.prefix = this.toString;
 }
 
 function Variable(name) {
-    this.toString = () => name
-    //:NOTE: difficult to extend
-    this.evaluate = (x, y, z) => name === "x" ? x : name === "y" ? y : z
-    this.prefix = this.toString
+    this.toString = () => name;
+    this.evaluate = (...args) => args[variables.get(name)];
+    this.prefix = this.toString;
 }
 
 function AbstractOperation(name, f, n, string, ...args) {
-    operations.set(string, [name, n, f])
-    this.getName = () => string
-    //:NOTE: this.args = args;
-    this.args = new Array(...args)
+    operations.set(string, [name, n, f]);
+    this.getName = () => string;
+    this.args = args;
 }
 
 AbstractOperation.prototype.toString = function () {
-    return ArrToStr(this.args.map(i => i.toString() + " ")) + this.getName()
+    return `${this.args.join(" ")} ${this.getName()}`;
 }
 AbstractOperation.prototype.prefix = function () {
-    return `(${this.getName()}${ArrToStr(this.args.map(i => " " + i.prefix()))})`
+    return `(${this.getName()} ${this.args.map(i => i.prefix()).join(" ")})`;
 }
-AbstractOperation.prototype.evaluate = function (x, y, z) {
-    return operations.get(this.getName())[2](...(this.args.map(i => i.evaluate(x, y, z))))
-}
-
-
-//:NOTE: Хочется, чтобы добавление операции выглядело так:
-// const Sin = createOperation(1, 'sin', Math.sin);
-// А не паста по типу .call и .prototype
-function Add() {
-    AbstractOperation.call(this, Add, (left, right) => left + right, 2, "+", ...arguments)
+AbstractOperation.prototype.evaluate = function (...args) {
+    return operations.get(this.getName())[2](...(this.args.map(i => i.evaluate(...args))));
 }
 
-Add.prototype = Object.create(AbstractOperation.prototype)
-
-function Subtract() {
-    AbstractOperation.call(this, Subtract, (left, right) => left - right, 2, "-", ...arguments)
+function createOperation(f, n, string) {
+    const result = function () {
+        AbstractOperation.call(this, result, f, n, string, ...arguments);
+    }
+    result.prototype = Object.create(AbstractOperation.prototype);
+    return result;
 }
 
-Subtract.prototype = Object.create(AbstractOperation.prototype)
-
-function Multiply() {
-    AbstractOperation.call(this, Multiply, (left, right) => left * right, 2, "*", ...arguments)
-}
-
-Multiply.prototype = Object.create(AbstractOperation.prototype)
-
-function Divide() {
-    AbstractOperation.call(this, Divide, (left, right) => left / right, 2, "/", ...arguments)
-}
-
-Divide.prototype = Object.create(AbstractOperation.prototype)
-
-function Negate() {
-    AbstractOperation.call(this, Negate, (operand) => -operand, 1, "negate", ...arguments)
-}
-
-Negate.prototype = Object.create(AbstractOperation.prototype)
-
-function Exp() {
-    AbstractOperation.call(this, Exp, (operand) => Math.exp(operand), 1, "exp", ...arguments)
-}
-
-Exp.prototype = Object.create(AbstractOperation.prototype)
-
-function Ln() {
-    AbstractOperation.call(this, Ln, (operand) => Math.log(operand), 1, "ln", ...arguments)
-}
-
-Ln.prototype = Object.create(AbstractOperation.prototype)
-
+const Add = createOperation((a, b) => a + b, 2, "+");
+const Subtract = createOperation((a, b) => a - b, 2, "-");
+const Multiply = createOperation((a, b) => a * b, 2, "*");
+const Divide = createOperation((a, b) => a / b, 2, "/");
+const Negate = createOperation(a => -a, 1, "negate");
+const Exp = createOperation(a => Math.exp(a), 1, "exp");
+const Ln = createOperation(a => Math.log(a), 1, "ln");
 const parse = expression => {
-    expression = expression.split(' ').filter(part => part.length > 0)
-    let stack = []
+    expression = expression.split(' ').filter(part => part.length > 0);
+    let stack = [];
     expression.map(s => {
-        //:NOTE: s in operations
-            if (operations.get(s) !== undefined) {
-                let elems = stack.slice(-operations.get(s)[1])
-                stack = stack.slice(0, -operations.get(s)[1])
-                stack.push(new (operations.get(s)[0])(...elems))
-            } else if (s === "x" || s === "y" || s === "z") {
-                stack.push(new Variable(s))
+            if (operations.has(s)) {
+                let elems = stack.slice(-operations.get(s)[1]);
+                stack = stack.slice(0, -operations.get(s)[1]);
+                stack.push(new (operations.get(s)[0])(...elems));
+            } else if (variables.has(s)) {
+                stack.push(new Variable(s));
             } else {
-                stack.push(new Const(+s))
+                stack.push(new Const(+s));
             }
         }
     )
-    return stack.pop()
+    return stack.pop();
 }
 const parsePrefix = string => {
     string = string.replaceAll("(", " ( ").replaceAll(")", " ) ").split(" ").filter(part => part.length > 0);
@@ -119,26 +78,29 @@ const parsePrefix = string => {
             } else if (element === "(") {
                 balance--;
                 if (balance < 0) {
-                    throw new ParsingError(element, index, "Problem with brackets");
+                    throw new BracketsError(element, index, `missing ")"`);
                 }
                 if (operationFlag) {
-                    throw  new ParsingError(element, index, `Expect operation, but found ${stack[stack.length - 1]}`);
+                    throw new InvalidElementError(element, index, `Expect operation, but found "${stack[stack.length - 1]}"`);
                 }
             } else if (operations.has(element)) {
                 let elems = stack.slice(stack.lastIndexOf(")", -operations.get(element)[1]) + 1, stack.length).reverse();
-                if (elems.length !== operations.get(element)[1]) {
-                    throw new ParsingError(element, index, "Not enough operands");
+                if (elems.length < operations.get(element)[1]) {
+                    throw new InvalidNumberOfArgumentsError(element, index, `Not enough operands: expect ${operations.get(element)[1]}, but found ${elems.length}`);
+                }
+                if (elems.length > operations.get(element)[1]) {
+                    throw new InvalidNumberOfArgumentsError(element, index, `Too many operands: expect ${operations.get(element)[1]}, but found ${elems.length}`);
                 }
                 stack = stack.slice(0, -operations.get(element)[1] - 1);
-                stack.push(new (operations.get(element)[0])(...elems))
+                stack.push(new (operations.get(element)[0])(...elems));
                 operationFlag = false;
-            } else if (element === "x" || element === "y" || element === "z") {
+            } else if (variables.has(element)) {
                 stack.push(new Variable((element), index));
             } else {
                 if (!isNaN(element)) {
                     stack.push(new Const(Number.parseInt(element)));
                 } else {
-                    throw new ParsingError(element, index, `Unexpected element ${element}`)
+                    throw new InvalidElementError(element, index, `Unexpected element ${element}`);
                 }
             }
 
@@ -147,19 +109,43 @@ const parsePrefix = string => {
     )
     stack = stack.filter(t => !(t === "(" || t === ")"));
     if (stack.length === 0) {
-        throw new ParsingError(undefined, 0, "No elements")
+        throw new InvalidInputError("No elements");
     }
     if (stack.length > 1) {
-        throw new ParsingError(stack[0], 0, "Too many arguments")
+        throw new InvalidNumberOfArgumentsError(stack[0], 0, "Too many arguments");
     }
     if (balance !== 0) {
-        throw new ParsingError(stack[0], 0, "Problem with brackets");
+        throw new BracketsError(stack[0], 0, `missing "("`);
     }
     return stack[0];
 };
 
 class ParsingError extends Error {
-    constructor(token, index, message) {
-        super(`${message} in element "${token}" at index ${index}`);
+    constructor(element, index, message) {
+        super(`${message} in element "${element}" at index ${index}`);
+    }
+}
+
+class BracketsError extends ParsingError {
+    constructor(element, index, message) {
+        super(element, index, `Problem with brackets: ${message}`);
+    }
+}
+
+class InvalidElementError extends ParsingError {
+    constructor(element, index, message) {
+        super(element, index, message);
+    }
+}
+
+class InvalidNumberOfArgumentsError extends ParsingError {
+    constructor(element, index, message) {
+        super(element, index, message);
+    }
+}
+
+class InvalidInputError extends Error {
+    constructor(message) {
+        super(`Incorrect input: ${message}`);
     }
 }
