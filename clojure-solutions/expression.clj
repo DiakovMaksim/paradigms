d83c443075dfb0a5853ede9e0c38cd4f0304fa03
Diakov Maksim <maksim2004d@yandex.ru>
2023-05-04 15:23:36 +0300

    Small fixes

--- clojure-solutions/expression.clj
+++ clojure-solutions/expression.clj
@@ -28,22 +28,21 @@
 (def rms (operation rmsEval))
 (def arcTan (operation #(Math/atan %)))
 (def arcTan2 (operation #(Math/atan2 %1 %2)))
-(def funcSymbols {'const constant, 'var variable, '+ add, '- subtract, '* multiply, '/ divide, 'negate negate, ;base
-                  'sum sum, 'avg avg, 'pow pow, 'log log, 'exp exp, 'ln ln, 'atan arcTan, 'atan2 arcTan2, ;extra
+(def funcSymbols {'const  constant, 'var variable, '+ add, '- subtract, '* multiply, '/ divide, 'negate negate, ;base
+                  'sum    sum, 'avg avg, 'pow pow, 'log log, 'exp exp, 'ln ln, 'atan arcTan, 'atan2 arcTan2, ;extra
                   'sumexp sumexp, 'lse lse, 'meansq meansq, 'rms rms}) ;hard
 (def _name (field :name))
 (def _args (field :args))
 (def _f (field :f))
 (def toString (method :toString))
 (def evaluate (method :evaluate))
-(def Operation {
-                :toString       (fn [this] (str "(" (_name this) " " (clojure.string/join " " (map #(toString %) (_args this))) ")"))
-                :evaluate       (fn [this vars]
-                                  (apply (_f this) (map #(evaluate % vars) (_args this))))})
+(def Operation {:toString (fn [this] (str "(" (_name this) " " (clojure.string/join " " (map #(toString %) (_args this))) ")"))
+                :evaluate (fn [this vars]
+                            (apply (_f this) (map #(evaluate % vars) (_args this))))})
 (def OperationConstructor (constructor (fn [this, f, name, args]
                                          (assoc this :f f :name name :args args)) Operation))
-(def ConstantPrototype {:evaluate       (fn [this _] (_args this))
-                        :toString       (fn [this] (str (_args this)))})
+(def ConstantPrototype {:evaluate (fn [this _] (_args this))
+                        :toString (fn [this] (str (_args this)))})
 (def ConstantConstructor (constructor (fn [this, value] (assoc this :args value)) ConstantPrototype))
 (defn Constant [value] (ConstantConstructor value))
 (defn Add [& args] (OperationConstructor +, "+", args))
@@ -65,16 +64,15 @@
 (defn ArcTan2 [& args] (OperationConstructor #(Math/atan2 %1 %2), "atan2", args))
 (defn Sin [& args] (OperationConstructor #(Math/sin %), "sin", args))
 (defn Cos [& args] (OperationConstructor #(Math/cos %), "cos", args))
-(def VariablePrototype {:evaluate       (fn [this variables] (variables (_name this)))
-                        :toString       (fn [this] (_name this))})
+(def VariablePrototype {:evaluate (fn [this variables] (variables (_name this)))
+                        :toString (fn [this] (_name this))})
 (def VariableConstructor (constructor (fn [this, name] (assoc this :name name)) VariablePrototype))
 (defn Variable [name] (VariableConstructor name))
 (def objSymbols {'const  Constant, 'var Variable, '/ Divide, 'negate Negate, '* Multiply, '- Subtract, '+ Add,
                  'sum    Sum, 'avg Avg, 'pow Pow, 'log Log, 'exp Exp, 'ln Ln, 'atan ArcTan, 'atan2 ArcTan2, ;extra
                  'sumexp Sumexp, 'lse LSE, 'meansq Meansq, 'rms RMS, 'sin Sin, 'cos Cos}) ;hard
-(defn parse [input operationSet] (
-                                            cond (number? input) ((operationSet 'const) input)
-                                                 (list? input) (apply (get operationSet (first input)) (mapv #(parse % operationSet) (rest input)))
-                                                 :else ((operationSet 'var) (str input))))
+(defn parse [input operationSet] (cond (number? input) ((operationSet 'const) input)
+                                       (list? input) (apply (get operationSet (first input)) (mapv #(parse % operationSet) (rest input)))
+                                       :else ((operationSet 'var) (str input))))
 (defn parseFunction [expr] (parse (read-string expr) funcSymbols))
 (defn parseObject [expr] (parse (read-string expr) objSymbols))
